c4eeadd7f0b22bdd6e28aa01a700b0baff0c0e0a
I am GAY AS FUCK <gster1234512345@mail.com>
2019-10-22 01:36:08 +0000

    added index html

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
+
+const requestHandler = (req, res) => {
+    //serve page under
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(PORT, (err) => {
+    if (err) return console.log('something bad happen', err)
+
+    console.log(`server is listening on ${PORT}`);
+})
